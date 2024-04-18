a3114d661b18fb5f9be36db0bb39aba0a9ca3923
Meyer <laurynemma@yahoo.com>
2024-04-18 14:16:32 -0400

    Added HelloWorld script - Wiki Stbl

--- Sandbox.js
+++ Sandbox.js
@@ -1,10 +1,16 @@
 // Programmer: Lauryn Meyer
 // Team Member: 
-// Bolt ID:
+// Bolt ID: SB-CF6D
 // Date: 4-16-2024
 // Program: Hello World - Sandbox
 // Program URL: https://edu.sphero.com/program/16700440/edit
 
+
 async function startProgram() {
-	// Write code here
+	await speak("Hello World", true);
+	setMainLed({ r: 0, g: 0, b: 255});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
+
 }
