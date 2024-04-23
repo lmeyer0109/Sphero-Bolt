cba8b1c68a71fbb47647927ddf602a91d1d79da7
Meyer <laurynemma@yahoo.com>
2024-04-23 13:35:42 -0400

    Initial My MiniCourse p2 commit

---
+++ MyMiniCourse.js
@@ -0,0 +1,11 @@
+// Programmer: Lauryn Meyer
+// Team Member: 
+// Bolt ID: SB-CF6D
+// Date: 4-23-2024
+// Program: My Mini Course
+// Program URL: https://edu.sphero.com/program/16743353/edit
+
+
+async function startProgram() {
+	
+}
