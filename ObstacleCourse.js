df8a1848b92b91bd6ed0e03bc6810a68195ce560
Meyer <laurynemma@yahoo.com>
2024-04-24 14:18:29 -0400

    Obstacle Course P2 Checkpoint 2

--- ObstacleCourse.js
+++ ObstacleCourse.js
@@ -9,4 +9,12 @@
 async function startProgram() {
 	await roll(0,70,2.25)	
 	await delay (1);
+
+// CheckPoint 2
+// Programer 2 : Ash Cowne
+
+	await roll((getHeading() - 70), 60, 2.5);
+	await delay (1);
+	await roll((getHeading() - 90), 60, 1);
+	await delay (1);
 }
