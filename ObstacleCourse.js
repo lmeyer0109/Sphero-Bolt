0a253491bc35a59dd2eb2bc058439c88f7351659
Meyer <laurynemma@yahoo.com>
2024-04-24 13:51:45 -0400

    Obstacle Course Checkpoint 1

--- ObstacleCourse.js
+++ ObstacleCourse.js
@@ -4,3 +4,9 @@
 // Date: 4-24-2024
 // Program: Obstacle Course (prelim)
 // Program URL: https://edu.sphero.com/program/16752489/edit
+
+// Programmer 1 : Lauryn
+async function startProgram() {
+	await roll(0,70,2.25)	
+	await delay (1);
+}
