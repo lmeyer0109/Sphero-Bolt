9ed6fb17a97a870ba1ce169359e91042b2c955e0
Meyer <laurynemma@yahoo.com>
2024-04-30 13:28:29 -0400

    added beginning comments & start of pgrm.js

---
+++ MazeCompetition.js
@@ -0,0 +1,10 @@
+// Programmer: Lauryn Meyer
+// Team Member: Ash Cowne
+// Bolt ID: SB-CF6D
+// Date: 4-30-2024
+// Program: Maze Competition Course
+// Program URL:https://edu.sphero.com/program/16788336/edit
+
+async function startProgram() {
+	// Write code here
+}
