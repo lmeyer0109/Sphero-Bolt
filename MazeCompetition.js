7677d0f0bde27b013f7607d731cdcf1c6b6840c8
Meyer <laurynemma@yahoo.com>
2024-05-01 13:54:33 -0400

    Programmer: Lauryn Meyer - Red LED Checkpoint Commit - Date: 5.1.2024
    edited last checkpoint going too long - made shorter distance

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -13,20 +13,24 @@ async function startProgram() {
 	await scrollMatrixText('Start!', { r: 66, g: 56, b: 255 }, 90, true)
 	await delay(1)
 	await speak("Start", true);
-	// Programmer: Lauryn Meyer - Blue LED Checkpoint Commit - Date: 5.1.2024
+	// Programmer: Lauryn Meyer - Blue LED Checkpoint Commit - Date:
 
 	await roll((getHeading() + 0), 75, 2.25);
 	await delay(1);
 	setMainLed({ r: 0, g: 0, b: 100 });
+
 	// Programer: Ash Cowne  - Sound One Checkpoint Commit - Date: 5.1.2024
-	
-	await roll((getHeading() + 90), 80, 1.5);
+	await roll((getHeading() + 90), 75,1.4);
 	await delay(1);
 	await Sound.Effects.Fairy.play()
-	// Programmer: ____ - Red LED Checkpoint Commit - Date:
-
-
-
+	
+	// Programmer: Lauryn Meyer - Red LED Checkpoint Commit - Date: 5.1.2024
+	await roll((getHeading() + 90), 75,1);
+	await delay(1);
+	await roll((getHeading() + 40),75,1);
+	await delay(.5);
+	setMainLed({ r: 100, g: 0, b: 0 });
+	
 	// Programmer: ____ - Sound Two Checkpoint Commit - Date:
 
 
