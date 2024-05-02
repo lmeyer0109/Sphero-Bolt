52f7359047f89743423d7390683d5d8fd58d4c27
Meyer <laurynemma@yahoo.com>
2024-05-02 13:50:47 -0400

    Programer: Ash Cowne - Sound Two Checkpoint Commit - Date: 5.2.2024
    Edited Red LED commit as well, made distance slower

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -6,35 +6,40 @@
 // Program URL:https://edu.sphero.com/program/16788336/edit
 
 async function startProgram() {
-	
-	// Programmer: Ash Cowne - Start Text & Audio Checkpoint Commit - Date:4.30.2024
-
- 
+	// Programer: Ash Cowne - Start Text & Audio Checkpoint Commit - Date: 4.30.2024
 	await scrollMatrixText('Start!', { r: 66, g: 56, b: 255 }, 90, true)
 	await delay(1)
 	await speak("Start", true);
-	// Programmer: Lauryn Meyer - Blue LED Checkpoint Commit - Date:
+	
+
+	// Programmer: Lauryn Meyer - Blue LED Checkpoint Commit - Date: 5.1.2024
 
 	await roll((getHeading() + 0), 75, 2);
 	await delay(1);
 	setMainLed({ r: 0, g: 0, b: 100 });
 
-	// Programer: Ash Cowne  - Sound One Checkpoint Commit - Date: 5.1.2024
-	await roll((getHeading() + 90), 75,1.4);
+// Programer: Ash Cowne  - Sound One Checkpoint Commit - Date: 5.1.2024
+	await roll((getHeading() + 90), 77,1.4);
 	await delay(1);
 	await Sound.Effects.Fairy.play()
 	
 	// Programmer: Lauryn Meyer - Red LED Checkpoint Commit - Date: 5.1.2024
-	await roll((getHeading() + 90), 75,1);
+	await roll((getHeading() + 90), 80,0.8);
 	await delay(1);
-	await roll((getHeading() + 40),75,1);
-	await delay(.5);
+	await roll((getHeading() + 40),70,1);
+	await delay(1);
 	setMainLed({ r: 100, g: 0, b: 0 });
+	await delay(1);
+	
 
-	// Programmer: ____ - Sound Two Checkpoint Commit - Date:
-
-
-
+	// Programer: Ash Cowne - Sound Two Checkpoint Commit - Date: 5.2.2024
+	await roll((getHeading() - 90), 70,0.6);
+	await delay(1);
+	await roll((getHeading() - 90), 75,1);
+	await delay(1);
+	await roll((getHeading() + 90), 60,0.7);
+	await Sound.Ambience.Zoo.play()
+	
 	// Programmer: ____ - Green LED Checkpoint Commit - Date:
 
 
