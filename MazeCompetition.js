c74948bd6c608da9598f562f8dd44b4c6325c50c
Meyer <laurynemma@yahoo.com>
2024-05-07 13:58:08 -0400

    Purple LED & Sound Three Checkpoint Commit - Date:5.7.2024

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -5,6 +5,14 @@
 // Program: Maze Competition Course
 // Program URL:https://edu.sphero.com/program/16788336/edit
 
+// Programmer: Lauryn Meyer
+// Team Member: Ash Cowne
+// Bolt ID: SB-CF6D
+// Date: 4-30-2024
+// Program: Maze Competition Course
+// Program URL:https://edu.sphero.com/program/16788336/edit
+
+
 async function startProgram() {
 	// Programer: Ash Cowne - Start Text & Audio Checkpoint Commit - Date: 4.30.2024
 	await scrollMatrixText('Start!', { r: 66, g: 56, b: 255 }, 90, true)
@@ -14,7 +22,7 @@ async function startProgram() {
 
 	// Programmer: Lauryn Meyer - Blue LED Checkpoint Commit - Date: 5.1.2024
 
-	await roll((getHeading() + 0), 75, 1.9);
+	await roll((getHeading() + 0), 75, 2);
 	await delay(1);
 	setMainLed({ r: 0, g: 0, b: 100 });
 
@@ -33,31 +41,35 @@ async function startProgram() {
 	
 
 	// Programer: Ash Cowne - Sound Two Checkpoint Commit - Date: 5.2.2024
-	await roll((getHeading() - 90), 65,1);
+	await roll((getHeading() - 90), 70,0.6);
 	await delay(1);
-	await roll((getHeading() - 90), 55,1);
+	await roll((getHeading() - 90), 75,1);
 	await delay(1);
-	await roll((getHeading() + 90), 60,0.8);
+	await roll((getHeading() + 90), 62,0.8);
 	await Sound.Effects.Fireworks.play()
 	
 	// Programmer: Lauryn Meyer - Green LED Checkpoint Commit - Date: 5.2.2024
+	//await delay (1);
+	//await roll((getHeading() + 0), 70,0.4);
 	await delay(1);
-	await roll((getHeading() + 90), 75,.5);
+	await roll((getHeading() + 90), 75,.59);
 	await delay(1);  
 	await roll((getHeading() - 90), 70,0.5);
 	setMainLed({ r: 0, g: 100, b: 0 });
 
-	// Programer: Ash Cowne - Purple LED & Sound Three Checkpoint Commit - Date:
+	// Programer: Ash Cowne - Purple LED & Sound Three Checkpoint Commit - Date:5.7.2024
 	await delay(1);
 	await roll((getHeading() + 0), 60,0.5);
 	await delay(1);
-	await roll((getHeading() - 90), 6,1);
+	await roll((getHeading() - 90), 70,1);
 	await delay(1);
-	await roll((getHeading() - 45), 70,0.5);
-	//await roll((getHeading() + 45), 60,0.5);
-
+	await roll((getHeading() - 45), 70,0.8);
+	await delay(1);
+	await roll((getHeading() + 45), 60,0.5);
+	await delay(1);
 	setMainLed({ r: 50, g:0, b: 50 });
 	await Sound.Effects.JokeDrum.play()
+
 
 
 
